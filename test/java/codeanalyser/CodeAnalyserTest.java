13ae021091ffcffde4cc4e424208f60c8a1506bd
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-04 17:25:06 +0530

    Added: Raise Custom Exception if Delimetrs is Wrong

--- test/java/codeanalyser/CodeAnalyserTest.java
+++ test/java/codeanalyser/CodeAnalyserTest.java
@@ -9,6 +9,7 @@ public class CodeAnalyserTest {
     private static final String INDIA_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
     private static final String WRONG_CODE_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
     private static final String CODE_CSV_FILE_WRONG_TYPE = "./src/test/resources/IndiaStateCode.pdf";
+    private static final String WRONG_Delimeter_FILE = "./src/test/resources/IndiaStateCode.csv";
 
 
     @Test
@@ -40,6 +41,18 @@ public class CodeAnalyserTest {
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CodeAnalyserException.class);
             codeAnalyser.loadIndiaCodeData(CODE_CSV_FILE_WRONG_TYPE);
+        } catch (CodeAnalyserException e) {
+            Assert.assertEquals(CodeAnalyserException.ExceptionType.CODE_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenIndiaCodeData_WithWrongDelimeter_ShouldThrowException() {
+        try {
+            CodeAnalyser codeAnalyser = new CodeAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CodeAnalyserException.class);
+            codeAnalyser.loadIndiaCodeData(WRONG_Delimeter_FILE);
         } catch (CodeAnalyserException e) {
             Assert.assertEquals(CodeAnalyserException.ExceptionType.CODE_FILE_PROBLEM,e.type);
         }
