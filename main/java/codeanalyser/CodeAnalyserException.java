af3477d3e734cae979baef8568ab241aee501304
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-04 16:55:31 +0530

    Added : Load Indian Stete Code Info from CSV file

---
+++ main/java/codeanalyser/CodeAnalyserException.java
@@ -0,0 +1,21 @@
+package codeanalyser;
+
+public class CodeAnalyserException extends Exception{
+
+    enum ExceptionType {
+        CODE_FILE_PROBLEM
+    }
+
+    ExceptionType type;
+
+    public CodeAnalyserException(String message, ExceptionType codeFileProblem) {
+        super(message);
+        this.type = type;
+    }
+
+    public CodeAnalyserException(String message, CodeAnalyserException.ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+
+}
