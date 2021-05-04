3e06c627eb9ff6f12bd7b5b8e826a4d1ff4e1c5a
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-04 16:59:39 +0530

    Added: Check the Number of records from CSV file Matched

---
+++ test/java/codeanalyser/CodeAnalyserTest.java
@@ -0,0 +1,22 @@
+package codeanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class CodeAnalyserTest {
+
+    private static final String INDIA_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
+    private static final String WRONG_CODE_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
+
+
+    @Test
+    public void givenIndianCodeCSVFileReturnsCorrectRecords() {
+        try {
+            CodeAnalyser codeAnalyser = new CodeAnalyser();
+            int numOfRecords = codeAnalyser.loadIndiaCodeData(INDIA_CODE_CSV_FILE_PATH);
+            Assert.assertEquals(37,numOfRecords);
+        } catch (CodeAnalyserException e) { }
+          catch (NullPointerException e){}
+    }
+}
